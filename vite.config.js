f97c25afb672a2d9f5f6b4347107d5eda2fe0209
Pgnop <penguingnop@gmail.com>
2025-11-17 14:53:24 +0800

    changes

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  // This repo will be served at: https://signaajames.github.io/forms
+  // So we set the base path to `/forms/`
+  base: '/forms/',
+});
