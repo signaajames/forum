8b323db4f8a41ba452c7db592497bf3833c34d5b
Pgnop <penguingnop@gmail.com>
2025-11-18 10:02:32 +0800

    changed css

--- orange/main.js
+++ orange/main.js
@@ -3,14 +3,17 @@ import './style.css'
 document.querySelector('#orange').innerHTML = /*html*/`
   <div class="page">
     <div class="answer-box">
-      <input
-        class="answer-input"
-        type="text"
-        placeholder="Abc"
-      />
-      <div class="answer-hint">
-        Press <span class="key">↵ Enter</span> to submit the <span class="key">code</span>
-      </div>
+      <section id='name'>
+        <div class='textarea'>
+          <h3><span class='req'>*</span> What's your Discord username?</h3>
+          <p class='desc'>Type your Discord username below</p>
+        </div>
+        <input
+          class="answer-input"
+          type="text"
+          placeholder="Abc"
+        />
+      </section>
     </div>
   </div>
 `
